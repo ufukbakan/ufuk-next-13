5d0a35e20b0e2e600d5fd4dd5b772fb7aeaec249
Ufuk Bakan <println.ufukbakan@gmail.com>
2023-03-14 00:36:25 +0300

    fix active-card zooming

--- src/components/FloatCard.tsx
+++ src/components/FloatCard.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/Menu.module.css";
-import { CSSProperties, ReactNode, useEffect, useState } from "react";
+import { CSSProperties, ReactNode, useEffect, useRef, useState } from "react";
 
 type FloatCardProps = {
     children?: ReactNode,
@@ -11,25 +11,38 @@ export default function (props: FloatCardProps) {
     const [styleList, setStyleList] = useState([
         styles["float-card-link"]
     ]);
+    const [customStyle, setCustomStyle] = useState<CSSProperties>({
+        "--bg-image": `url('${props.bg}')`
+    } as CSSProperties);
+    const element = useRef<HTMLAnchorElement>(null);
 
     useEffect(() => {
-        if (props.active) {
+        if (props.active && element.current) {
             setStyleList([
                 styles["float-card-link"],
                 styles["active-card"]
             ]);
-        }else{
+            const shouldInX = global.window?.innerWidth / 2 - (element.current.getBoundingClientRect().width / 2);
+            const shouldInY = global.window?.innerHeight / 2 - (element.current.getBoundingClientRect().height / 2);
+            const diffToX = (shouldInX - element.current.getBoundingClientRect().x);
+            const diffToY = (shouldInY - element.current.getBoundingClientRect().y);
+            setCustomStyle({
+                ...customStyle,
+                "transform": `translate(${diffToX}px, ${diffToY}px) scale(4)`
+            })
+        } else {
             setStyleList([
                 styles["float-card-link"]
             ]);
+            setCustomStyle({
+                "--bg-image": `url('${props.bg}')`
+            } as CSSProperties);
         }
-    }, [props.active]);
+    }, [props.active, global.window]);
 
     return (
-        <a href={props.href} className={styleList.join(" ")}>
-            <div className={styles["float-card"]} style={{
-                "--bg-image": `url('${props.bg}')`
-            } as CSSProperties} >
+        <a ref={element} href={props.href} className={styleList.join(" ")} style={customStyle}>
+            <div className={styles["float-card"]}>
                 {props.children}
             </div>
         </a>
