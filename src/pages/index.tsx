d43e4ffd654c06737cfba198c2f1cd68ac67cecf
ufukbakan <println.ufukbakan@gmail.com>
2023-03-15 09:14:54 +0300

    fix scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,6 @@
-import MainMenu from "@/components/MainMenu";
-import Nav from "@/components/Nav";
-import dynamic from "next/dynamic";
-
-
 export default function Home() {
-  
+
   return (
-    <div className="scrollable-content">
-      <></>
-    </div>
+    <></>
   )
 }
