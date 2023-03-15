41930fa02a0fd564e842c1252ec508b5b84ce001
ufukbakan <println.ufukbakan@gmail.com>
2023-03-15 10:10:44 +0300

    increase typing speed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import BackButton from './BackButton';
 import TypedText from "./TypedText";
 
 export default function () {
-    const content = TypedText("Ufuk Bakan, a software developer from Turkey, was born in Çorum. Have worked on many different subjects including AI, ML, cryptology, big data & web development.", 1.5, 1);
+    const content = TypedText("Ufuk Bakan, a software developer from Turkey, was born in Çorum. Have worked on many different subjects including AI, ML, cryptology, big data & web development.", 2, 1);
 
     return (
         <div className={styles['section']} id="about">
