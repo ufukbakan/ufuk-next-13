0f73e00660b8a574d1d0cf0c23a32a3ae96b81e7
ufukbakan <println.ufukbakan@gmail.com>
2023-03-16 10:29:55 +0300

    update fadecomponent

--- src/components/Fade.tsx
+++ src/components/Fade.tsx
@@ -1,10 +1,13 @@
-import { FunctionComponent, ReactNode, useEffect, useRef, useState } from "react";
 import styles from "@/styles/Fade.module.css";
+import { FunctionComponent, ReactNode } from "react";
 
 export function fadeComponent(Component: FunctionComponent, delay = 0) {
 
     return (
-        <div style={{ animationDelay: delay + "s" }} className={styles.fading}>
+        <div style={{
+            animationDuration: 10 * delay / 7 + "s",
+            animationPlayState: "running"
+        }} className={styles.fading}>
             <Component />
         </div>
     )
